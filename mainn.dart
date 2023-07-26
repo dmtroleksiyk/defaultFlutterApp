f454be41eb06ea76d6b809427b5cef7c504230a9
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-07-26 09:41:31 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see thee
-        // application has a blue toolbar. Then, without quitting the app, try
+        // application has a blue toolbar. Then, without quitti
+        Arrested capital))))ng the app, try
         // changing the primarySwatch below to Colors.green and then invoke
 // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",

5442d8c10e39e1381998cbc492e2e6be8ea85c29
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-07-27 14:56:44 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -22,6 +22,10 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
          // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+       // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+         // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,f
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),

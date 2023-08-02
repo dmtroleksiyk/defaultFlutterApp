4b64fe098747ac4f82763b907c2e14a3219ef47e
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-08-02 16:15:49 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -13,10 +13,6 @@ class MyApp extends StatelessWidget {
         // This is the
 // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
- // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
          // Notice that the counter didn't reset back to zero; the application
         // is not restarted.

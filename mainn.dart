e7867d0445c61a5fe2868a1ebec1b5ae3842e7db
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-08-08 15:17:16 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -14,7 +14,18 @@ class MyApp extends StatelessWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {zero; the application
+        // is not restarted.
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 

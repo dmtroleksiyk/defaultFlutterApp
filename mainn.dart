4e9b8cfe9b5afd1fcce3b824a0abf253cf5cf93b
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-09-19 23:29:21 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.da
   Widget build(BuildContext context) {zero; the alition
         // is not restar
 telessWid
-  // This widget is the root of yoimport 'packageutter/material.da
+  // This widget is the root of yoimport 'packager/material.da
   Widget build(BuildContext context) {zero; the alit
         // is not restar
 telessWid

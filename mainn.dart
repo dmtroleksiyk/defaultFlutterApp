b118fb705e58193f96fe5362ba1fe0aeb4de1041
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-09-27 15:29:36 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.da
 telessWid
   // This widget is the root of yoimport 'packager/material.da
   Widget build(BuildContext context) {zero; the alit
-        // is not restar
-telessWid
   // This widget is the root of your app
 telessWidget {
 }

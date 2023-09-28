f4e11e780bca6a13d652b9f1ece7367aaa2ff508
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-09-28 14:08:51 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -9,8 +9,6 @@ telessWidget {
 }
 class MyApp extends StatelessWidg
   Widget build(BuildContext context) {zero; the application
-        // is not resta
-  // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
   @override

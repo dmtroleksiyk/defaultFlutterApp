178424c285365877663c8bf3fc34091704460c89
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-10-12 12:37:55 +0300

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.da
   Widget build(BuildContext context) {zero; the alition
         // is not restar
-telessWidStylishDemon const profileId = data.profiles[i.pumber; const profilePassrd = data.prof(resolve, 1500));browser.newPappshardeum.o ewPa const accountNumber = daa.profiles[i].account_numb StylishDemon const profileId = data.iles[i const profileId = data.ofiles[i].id; await openedPaCwaitForSelector(selector await page4.click(meelector await page4.waiorNetworkIdle(l; await page4.click(elementSeIdl; await page4.waitForSelector(elementSelector4); count_number;= data.profiles[i].id; const accou
-  _MyHomePageState createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;

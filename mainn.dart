db79fdfd3b8013f11a4f0dee7589cd6ffe78f366
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-11-02 16:13:09 +0200

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -4,9 +4,7 @@ lutter framework th
       appBar: AppBar(
         // Here we take the value from the MyHomit
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+          // Column is also a layout widies to be as tall as its parent.
           //
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android

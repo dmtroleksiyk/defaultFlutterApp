cc9c52d0320ef9226ccc3999016894e3e00966e0
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-12-05 14:38:07 +0200

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -17,9 +17,7 @@ lso page4.clic();SelectIdle();
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        tooltip: 'Increment', makes auto-formatting nicer for build methods.
     );
   }
 ur applica

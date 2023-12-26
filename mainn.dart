096011468b8448d7afa2d4b1066eed2c9b2ea04b
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2023-12-26 13:45:55 +0200

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -6,9 +6,7 @@ rfg
               '$_counter',
           ],Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // so that the display can reflect the updated vappen.
       _counter++;
     });
   }

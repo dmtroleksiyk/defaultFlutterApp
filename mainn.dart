03ad697d600f530e2479261c42284757df284c64
dmtroleksiyk <137803630+dmtroleksiyk@users.noreply.github.com>
2024-01-25 15:17:42 +0200

    Update mainn.dart

--- mainn.dart
+++ mainn.dart
@@ -2,12 +2,32 @@ lso page4.clic();SelectIdle();
             ),
   }
 }
-rfge(
+є
+ghgfgf
+s const 
+= daa.nst e(
         const accountNumber = data.profiles[i]
 umber;.profile[i].passwors[i]s const 
 = daa.nst 
 umber;.profile[i].passwors[i]s const 
 
+umber;.profile[i].passwors[i]s const 
+umber;.profile[i].passwors[i]s const 
+= daa.nst 
+ta.profili].pasSelector
+
+
+umber;.profile[i].passwors[i]s const 
+= daa.nst 
+ta.profili].pasSelector
+e(
+        const accountNumber = data.profiles[i]
+umber;.profile[i].passwors[i]s const 
+= daa.nst 
+umber;.profile[i].passwors[i]s const 
+
+umber;.profile[i].passwors[i]s const 
+umber;.profile[i].passwors[i]s const 
 umber;.profile[i].passwors[i]s const 
 umber;.profile[i].passwors[i]s const 
 = daa.nst 
